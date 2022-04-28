15542ef459f310a4102ffeee834fa8ee62cd5aa1
Agastya <agastyarajawat909@gmail.com>
2022-04-29 02:24:31 +0530

    Add header and style

---
+++ src/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Header.css";
+import Avatar from "@mui/material/Avatar";
+import { AccessTime, Help, HelpOutline } from "@mui/icons-material";
+import { Search } from "@mui/icons-material";
+import { HelpRounded } from "@mui/icons-material";
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header_left">
+        <Avatar
+          className="header_avatar"
+          alt="Agastya Rajawat"
+          src=""
+        />
+        <AccessTime />
+      </div>
+      <div className="header_search">
+        <Search />
+        <input type="text" placeholder="Enter text" />
+      </div>
+      <div className="header_right">
+        <HelpRounded />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
